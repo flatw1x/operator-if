1fedcfaafe25caed8d3e5084e958687078635e4e
flatw1x <vasiliy.vinokurov@yandex.ru>
2024-08-24 10:34:09 +0300

    Add files via upload

---
+++ operator_if.py
@@ -0,0 +1,10 @@
+print('Введите три числа')
+first = int(input())
+second = int(input())
+third = int(input())
+if first == second == third:
+    print(3)
+elif first == second or second == third or first == third:
+    print(2)
+else:
+    print(0)
